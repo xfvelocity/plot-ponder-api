18878ea9a307f9a26c67affd14a2ac28ea8ba054
Alex Long <alexlong2001@outlook.com>
2023-10-14 11:49:00 +0100

    added paginatied feed endpoint

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,13 +1,31 @@
 const Review = require("../models/review");
 
 const getFeed = async (req, res) => {
-  const uuid = req.params.uuid || req.user.uuid;
+  const page = parseInt(req.query.page || "") || 1;
+  const perPage = parseInt(req.query.perPage || "") || 10;
+  const query = {
+    "user.uuid": { $ne: req.user.uuid },
+  };
 
-  const reviews = await Review.find({});
+  try {
+    const total = await Review.countDocuments(query);
 
-  console.log(reviews);
+    const reviews = await Review.find(query)
+      .skip((page - 1) * perPage)
+      .limit(perPage);
 
-  res.send({});
+    res.json({
+      data: reviews,
+      meta: {
+        page,
+        perPage,
+        totalPages: Math.ceil(total / perPage),
+        total,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
 };
 
 module.exports = {

29169dc22783a685c570c5ea6cabfa69b2eae5a0
Alex Long <alexlong2001@outlook.com>
2023-10-15 13:44:44 +0100

    updated user reviews

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -48,10 +48,30 @@ const userProfile = async (req, res) => {
 
 const userReviews = async (req, res) => {
   const uuid = req.params.uuid || req.user.uuid;
+  const page = parseInt(req.query.page || "") || 1;
+  const perPage = parseInt(req.query.perPage || "") || 10;
+  const query = {
+    "user.uuid": uuid,
+  };
 
-  const reviews = await Review.find({ userUuid: uuid });
+  try {
+    const total = await Review.countDocuments(query);
+    const reviews = await Review.find(query)
+      .skip((page - 1) * perPage)
+      .limit(perPage);
 
-  res.send(reviews);
+    res.json({
+      data: reviews,
+      meta: {
+        page,
+        perPage,
+        totalPages: Math.ceil(total / perPage),
+        total,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
 };
 
 module.exports = {

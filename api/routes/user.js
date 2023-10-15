6470db0f399729d1976a9326f6fa22eba18fb4a8
Alex Long <alexlong2001@outlook.com>
2023-10-15 19:36:41 +0100

    fixed user endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,7 +6,7 @@ const { searchUser, userProfile, userReviews } = require("../controllers/user");
 router.get("/user-search", searchUser);
 
 router.get("/user", userProfile);
-router.get("/user/:uuid", userProfile);
+router.get("/user/:username", userProfile);
 
 router.get("/user-reviews", userReviews);
 router.get("/user-reviews/:uuid", userReviews);
